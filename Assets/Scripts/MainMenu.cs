a8386d3b5eee681182f271cb1da98df986169a79
wtsiwon <siwon4727@gmail.com>
2022-07-09 18:06:57 +0900

    폰트,닷트윈추가, 타이틀, 메인메뉴 만드는중 중간저장

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("게임 시작 버튼")]
+    [Header("게임 시작 버튼")]
+    private Button gameStartbtn;
+
+    [SerializeField]
+    [Tooltip("파워업 창")]
+    [Header("파워업 창")]
+    private Button powerUpBtn;
+
+    [SerializeField]
+    [Tooltip("뒤로가기")]
+    [Header("뒤로가기 버튼")]
+    private Button backBtn;
+
+    [SerializeField]
+    private GameObject powerUpBoard;
+
+
+    
+    private void Start()
+    {
+        gameStartbtn.onClick.AddListener(() =>
+        {
+            GameStart();
+        });
+        powerUpBtn.onClick.AddListener(() =>
+        {
+            PowerUpBoardActive();
+        });
+        backBtn.onClick.AddListener(() =>
+        {
+            backScene();
+        });
+    }
+    #region 파워업창 띄우는 함수
+    /// <summary>
+    /// 파워업창 띄우기
+    /// </summary>
+    private void PowerUpBoardActive()
+    {
+        powerUpBoard.SetActive(true);
+    }
+    #endregion
+    #region 타이틀로 가는 함수
+    /// <summary>
+    /// 다시 타이틀로 가는 함수
+    /// </summary>
+    private void backScene()
+    {
+        SceneManager.LoadScene("Title");
+    }
+    #endregion
+    #region 게임 시작 함수
+    private void GameStart()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+    #endregion
+    #region 게임 나가는 함수
+    ///// <summary>
+    ///// 게임 나가기
+    ///// </summary>
+    //private void GameQuit()
+    //{
+    //    Application.Quit();
+    //}
+    #endregion//주석
+}
